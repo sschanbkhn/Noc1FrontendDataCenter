674284df66d26724484acbba8e6ea4cb1a186f0e
sschanbkhn <sschanbkhn@gmail.com>
2025-08-28 00:10:20 +0700

    luu code fe chay tren dev o muc 80 den 90% 28082025_01210

--- src/components/ANM/UC1/Form/index.tsx
+++ src/components/ANM/UC1/Form/index.tsx
@@ -1,67 +1,58 @@
-import CtrlDynamicForm from 'components/common/CtrlDynamicForm';
-import CtrlNotification from 'components/common/CtrlNotification';
-import { IResponseMessage } from 'models/Apps';
-import { Message } from 'models/Enums';
-import React, { useEffect, useReducer, useRef, useState } from 'react'
+import CtrlDynamicForm from "components/common/CtrlDynamicForm";
+import CtrlNotification from "components/common/CtrlNotification";
+import { IResponseMessage } from "models/Apps";
+import { Message } from "models/Enums";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 import { connect } from "react-redux";
-import { InitState } from './InitState';
-import { Actions } from './Action';
-import { Reducer } from './Reducer';
-import devicesFormInputJson from './FormInput.json';
+import { InitState } from "./InitState";
+import { Actions } from "./Action";
+import { Reducer } from "./Reducer";
+import devicesFormInputJson from "./FormInput.json";
 interface Props {
-  Id: string,
-  ReloadTableItems: any
-  Options: any
+  Id: string;
+  ReloadTableItems: any;
+  Options: any;
 }
 
-const DevicesForm = (props: Props) => {  
-  const [state, dispatch] = useReducer(Reducer, InitState)
+const DevicesForm = (props: Props) => {
+  const [state, dispatch] = useReducer(Reducer, InitState);
   useEffect(() => {
     Actions.GetItem(props.Id, dispatch);
-  }, [props.Id])
-  let devicesFormInput:any = devicesFormInputJson;
+  }, [props.Id]);
+  let devicesFormInput: any = devicesFormInputJson;
   const refNotification = useRef<any>();
   const refDynamicForm = useRef<any>();
   const ActionEvents = {
     onClickSave: async () => {
       let isValid = refDynamicForm.current.onValidation();
-      if(isValid)
-      {        
+      if (isValid) {
         let stateValues = refDynamicForm.current.getStateValues();
-        let res:IResponseMessage = null;                
+        let res: IResponseMessage = null;
         res = await Actions.CheckDuplicateAttributes(stateValues.Id, stateValues.Code, dispatch);
-        if(res.Data) 
-        {
-          refNotification.current.showNotification("warning", Message.DuplicateAttribute_Code);    
-          return; 
-        }                           
-        if(props.Id) 
-        {          
-          res = await Actions.UpdateItem(stateValues);                      
-        }          
-        else
-        {
-          res = await Actions.CreateItem(stateValues);  
-        }           
-        if(res.Success) {            
-          refNotification.current.showNotification("success", res.Message);          
+        if (res.Data) {
+          refNotification.current.showNotification("warning", Message.DuplicateAttribute_Code);
+          return;
+        }
+        if (props.Id) {
+          res = await Actions.UpdateItem(stateValues);
+        } else {
+          res = await Actions.CreateItem(stateValues);
+        }
+        if (res.Success) {
+          refNotification.current.showNotification("success", res.Message);
           props.ReloadTableItems();
-        }                    
+        }
       }
     },
-  }
-  return(
+  };
+  return (
     <>
-      <CtrlNotification ref={refNotification} />   
+      <CtrlNotification ref={refNotification} />
       <CtrlDynamicForm options={props.Options} ref={refDynamicForm} initValues={state.DataItem} formDefs={devicesFormInput} actionEvents={ActionEvents} />
     </>
-  )
-}
-const mapState = ({ ...state }) => ({
-  
-});
-const mapDispatchToProps = {
-  
+  );
 };
+const mapState = ({ ...state }) => ({});
+const mapDispatchToProps = {};
 
 export default connect(mapState, mapDispatchToProps)(DevicesForm);
